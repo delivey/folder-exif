e0222ee9d73f05b0c3c160f40162a4d50d41b500
delivey <35540227+delivey@users.noreply.github.com>
2020-02-16 16:42:05 +0200

    v.1.01 minor optimizations

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ folder = input("""What directory is your folder located in? e.g: D:/Exif/Folder/
 """)
 file_list = os.listdir(folder)
 
-
 for file in file_list:
     if not file.endswith('.txt'):
-        f = open('%s%s' % (folder, file), 'rb')
-        tags = er.process_file(f)
-        strings = '%s' % tags
-        sep = ": b'"
-        rest = strings.split(sep, 1)[0]
-        txt = open("%s%s.txt" % (folder, file), "w")
-        txt.write("%s" % rest)
+        f = open(folder + file, 'rb')
+        tags = "%s" % er.process_file(f)
+        rest = tags.split(": b'", 1)[0]
+        txt = open(folder + file, 'w')
+        wrest = txt.write(rest)
         print("File %s log saved." % file)
+print("All done!")
 
